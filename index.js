724a8efeb9eea59fd173e9220bbc16a98583bc50
rahulfordev <rahulforsms@gmail.com>
2024-03-24 15:47:09 +0600

    user data get and post to database

--- index.js
+++ index.js
@@ -14,27 +14,45 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// MongoDB connection URI
-const uri = process.env.MONGO;
+// MongoDB connection URL
+const databaseURL = process.env.MONGO;
 
 // Create a MongoClient instance
-const client = new MongoClient(uri);
+const client = new MongoClient(databaseURL);
 
-async function accessCollection() {
+async function mainFunc() {
   try {
     // Connect to MongoDB
     await client.connect();
     console.log("Connected to MongoDB");
 
     // Access the database and collection
+    const database = client.db("doctorsPortal");
+    const usersCollection = database.collection("users");
+    const appointmentOptionCollection =
+      database.collection("appointmentOptions");
+    const bookingsCollection = database.collection("bookings");
+    const doctorsCollection = database.collection("doctors");
+    const paymentsCollection = database.collection("payments");
 
-    // Define routes
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      res.send(users);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.error(error);
   }
 }
 
-accessCollection();
+mainFunc().catch(console.log());
 
 // Serve index.html
 app.get("/", (req, res) => {
